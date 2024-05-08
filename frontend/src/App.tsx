bc28c333706a566ff2deccfa1e8fd83f178dfb45
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-05-08 16:34:03 +0200

    add frontend output songcard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
 import Startpage from "./pages/Startpage.tsx";
+import {useEffect, useState} from "react";
+import {Song} from "./model/Song.ts";
+import axios from "axios";
 
 function App() {
+    const[songList, setSongList] = useState<Song[]>([]);
+
+    function fetchSongs(){
+        axios.get('/api/song')
+            .then((response) => {
+                setSongList(response.data);
+            })
+    }
+
+    useEffect(() => {
+        fetchSongs();
+    }, [])
 
   return (
     <>
-      <Startpage/>
+      <Startpage songList={songList} />
     </>
   )
 }
--- frontend/src/pages/Startpage.tsx
+++ frontend/src/pages/Startpage.tsx
@@ -1,10 +1,20 @@
-export default function Startpage(){
+export default function Startpage({songList}){
     return (
         <>
         <h1>ChordMate</h1>
-        <div className="input-box">
-            <h2>Song hinzufügen</h2>
-
+        <div className="song-card">
+            <div className="input-box">
+                <h2>Song hinzufügen</h2>
+            </div>
+            <div className={"output-box"}>
+                {songList.map((song) => (
+                    <div className={"output-content"} key={song.id}>
+                        <h3>{song.title}</h3>
+                        <p className={"artist-paragraph"}>{song.artist}</p>
+                        <p>{song.text}</p>
+                    </div>
+                ))}
+            </div>
         </div>
         </>
     )
