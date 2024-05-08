4982baf6c898a8c9ddc480c81e13844e3ee05a45
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-05-08 09:43:06 +0200

    correct typo

--- backend/src/main/java/com/example/backend/controller/SongController.java
+++ backend/src/main/java/com/example/backend/controller/SongController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/song")
+@RequestMapping("/api/song")
 @RequiredArgsConstructor
 public class SongController {
 
