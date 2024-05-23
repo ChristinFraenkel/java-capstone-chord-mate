5b0aac50ff9b67fc731eb2394c111dbbdbd8910b
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-05-23 16:07:31 +0200

    Stand 23.4. 17:00

--- frontend/src/pages/Startpage.tsx
+++ frontend/src/pages/Startpage.tsx
@@ -1,24 +1,15 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { Song } from "../model/Song.ts";
 import { formatSongText, extractChords } from "../utils/utils.tsx";
 
 export default function Startpage({ songList, newSong, setNewSong }: { songList: Song[], newSong: Song, setNewSong: (song: Song) => void }) {
-
     const [filter, setFilter] = useState<string>("");
     const [chordFilter, setChordFilter] = useState<string[]>([]);
+    const [possibleChords, setPossibleChords] = useState<string[]>([]);
+    const [filteredSongList, setFilteredSongList] = useState<Song[]>(songList);
 
-    // Funktion, um alle einzigartigen Chords zu extrahieren
-    const getAllChords = (songs: Song[]): string[] => {
-        const allChords = new Set<string>();
-        songs.forEach(song => {
-            extractChords(song.text).forEach(chord => allChords.add(chord));
-        });
-        return Array.from(allChords);
-    };
-
-    const allChords = getAllChords(songList);
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -26,7 +17,7 @@ export default function Startpage({ songList, newSong, setNewSong }: { songList:
             .then((response) => { console.log(response); })
             .catch((error) => { console.log(error.message); });
         setNewSong({ id: "", artist: "", title: "", text: "" });
-    }
+    };
 
     function handleOnChange(event: ChangeEvent<HTMLInputElement>) {
         const key = event.target.name;
@@ -43,15 +34,36 @@ export default function Startpage({ songList, newSong, setNewSong }: { songList:
         setChordFilter(prev =>
             prev.includes(chord) ? prev.filter(c => c !== chord) : [...prev, chord]
         );
+        // console.log(chordFilter);
     };
 
-    const filteredSongList = songList.filter((song) => {
-        const chords = extractChords(song.text);
-        return (song.title.toLowerCase().includes(filter.toLowerCase()) ||
-                song.artist.toLowerCase().includes(filter.toLowerCase()) ||
-                song.text.toLowerCase().includes(filter.toLowerCase())) &&
-            (chordFilter.length === 0 || chordFilter.every(chord => chords.includes(chord)));
-    });
+    useEffect(() => {
+        const filtered = songList.filter((song) => {
+            return (
+                (song.title.toLowerCase().includes(filter.toLowerCase()) ||
+                    song.artist.toLowerCase().includes(filter.toLowerCase()) ||
+                    song.text.toLowerCase().includes(filter.toLowerCase()))
+            );
+        });
+        setFilteredSongList(filtered);
+
+    }, [filter, songList]);
+
+    useEffect(() => {
+        const filteredChords = songList.filter((song) => {
+            const chords = extractChords(song.text);
+            return (
+                (chordFilter.length === 0 || chordFilter.every(chord => chords.includes(chord)))
+            );
+        });
+        setFilteredSongList(filteredChords);
+
+        const allChords = new Set<string>();
+        filteredChords.forEach(song => {
+            extractChords(song.text).forEach(chord => allChords.add(chord));
+        });
+        setPossibleChords(Array.from(allChords));
+    }, [chordFilter]);
 
     return (
         <>
@@ -61,23 +73,44 @@ export default function Startpage({ songList, newSong, setNewSong }: { songList:
                     <h2>Song hinzufügen</h2>
                     <div className={"form-box"}>
                         <form onSubmit={handleSubmit}>
-                            <input type="text" name="title" id="title" placeholder={"Titel"} onChange={handleOnChange} value={newSong.title} />
-                            <input type="text" name="artist" id="artist" placeholder={"Künstler"} onChange={handleOnChange}
-                                   value={newSong.artist} />
-                            <textarea name="text" id="text" placeholder={"Songtext: Akkorde bitte mit eckigen Klammern einfügen -> [G]"} rows={10} cols={50} onChange={handleOnTextareaChange}
-                                      value={newSong.text} />
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                placeholder={"Titel"}
+                                onChange={handleOnChange}
+                                value={newSong.title}
+                            />
+                            <input
+                                type="text"
+                                name="artist"
+                                id="artist"
+                                placeholder={"Künstler"}
+                                onChange={handleOnChange}
+                                value={newSong.artist}
+                            />
+                            <textarea
+                                name="text"
+                                id="text"
+                                placeholder={"Songtext: Akkorde bitte mit eckigen Klammern einfügen -> [G]"}
+                                rows={10}
+                                cols={50}
+                                onChange={handleOnTextareaChange}
+                                value={newSong.text}
+                            />
                             <button type={"submit"}>hinzufügen</button>
                         </form>
                     </div>
                 </div>
                 <div className={"output-box"}>
                     <div className="filter-box">
-                        {allChords.map(chord => (
+                        {possibleChords.map(chord => (
                             <label key={chord}>
                                 <input
                                     type="checkbox"
                                     value={chord}
                                     onChange={handleChordFilterChange}
+                                    checked={chordFilter.includes(chord)}
                                 />
                                 {chord}
                             </label>
@@ -96,7 +129,7 @@ export default function Startpage({ songList, newSong, setNewSong }: { songList:
                             <div>
                                 {formatSongText(song.text).slice(0, 10)}{'...'}
                             </div>
-                            <Link to={`/detail/${song.id}`} >
+                            <Link to={`/detail/${song.id}`}>
                                 <button>Mehr anzeigen</button>
                             </Link>
                         </div>
